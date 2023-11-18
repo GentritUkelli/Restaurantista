720eab74b980bcd79c01ccb53f26ac7e03c38fb7
GentritUkelli <112003299+GentritUkelli@users.noreply.github.com>
2023-11-18 22:55:43 +0100

    no message

--- server.js
+++ server.js
@@ -1,65 +1,64 @@
-//Node modules HTTP
 const http = require("http");
-// fs=filesystem
 const fs = require("fs");
-// path module
 const path = require("path");
-const { URLSearchParams } = require("url");
 
-// create server
 const server = http.createServer((req, res) => {
-  if (req.method === `POST`) {
-    if (req.url === `/submit-form.js`) {
-      let body = ``;
+  if (req.method === "POST") {
+    if (req.url === "/submit-form.js") {
+      let body = "";
 
+      // Read the data from the request
       req.on("data", (chunk) => {
         body += chunk;
       });
 
+      // Process the data once it has been fully received
       req.on("end", () => {
+        // Parse the form data
         const formData = new URLSearchParams(body);
         const email = formData.get("email");
-        const newsletter = formData.get("newsletter") === `on`;
+        const newsletter = formData.get("newsletter") === "on";
 
         // Perform validation
         if (!isValidEmail(email)) {
           res.writeHead(400);
           res.end("Invalid email address");
         } else {
+          // Return a response to the client
           res.writeHead(200);
-          res.end("From submitted successfully");
+          res.end("Form submitted successfully");
         }
       });
     }
   } else {
-    let filePath = `.` + req.url;
+    let filePath = "." + req.url;
     if (filePath === "./") {
-      filePath = "./index.html";
+      filePath = "index.html";
     }
 
     const extname = path.extname(filePath);
-    let contentFilePath = `text/html`; 
+    let contentType = "text/html";
 
-    switch(extname) {
-      case `.css`:
-          contentType =  `text/css`;
-          break;
-      case `.js`:
-        contentType = `text/js`;
+    switch (extname) {
+      case ".css":
+        contentType = "text/css";
+        break;
+      case ".js":
+        contentType = "text/javascript";
         break;
     }
 
-    fs.readFileSync(filePath, (error, content) => {
+    fs.readFile(filePath, (error, content) => {
       if (error) {
-        if(error.code === 'ENOENT'){
+        if (error.code === "ENOENT") {
           res.writeHead(404);
-          res.end(`404 File not found:`);
-        } else{
+          res.end("404 Not Found");
+        } else {
           res.writeHead(500);
-          res.end(`500 Internal Server Error`);
+          res.end("500 Internal Server Error");
         }
-      }else{
-        res.writeHead(200, {"Content-Type": contentType});
+      } else {
+        res.writeHead(200, { "Content-Type": contentType });
         res.end(content, "utf-8");
       }
     });
@@ -71,7 +70,8 @@ server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-function isValidEmail(email){
-  var emailRegex = /\S+@\.\S+/;
+// Email validation function
+function isValidEmail(email) {
+  const emailRegex = /\S+@\S+\.\S+/;
   return emailRegex.test(email);
 }
