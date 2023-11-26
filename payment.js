595399d4872cff203569c0bc2078b13ff6184fbb
GentritUkelli <112003299+GentritUkelli@users.noreply.github.com>
2023-11-26 01:36:20 +0100

    no message

--- payment.js
+++ payment.js
@@ -15,22 +15,11 @@ paymentForm.addEventListener("submit", function(event){
       return;
     }
 
-    //validate card number cvc
-    //start in the beginning of string 
-    //consist on number 0-9 
-    //have a total from 13 16 digits 
-    //end at the end of the string  
     if(!cardNumber.match(/^\d{13,16}$/)){
        alert("Please enter a valid card number,");
        return;
     }
 
-    //validate cvc 
-    //start string
-    //this is the start
-    //only digits 0 to 9
-    //have a total from 3 4 digits 
-    // \d
     if(!cvc.match(/^\d{3,4}$/)){
       alert("Please enter a valid CVC");
       return;
